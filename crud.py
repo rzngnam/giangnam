86a355e8278aa5bd52ff80b18ac9c25b8c397c42
rzngnam <35531870+rzngnam@users.noreply.github.com>
2018-06-07 23:19:25 +0700

    Add files via upload

---
+++ crud.py
@@ -0,0 +1,24 @@
+action = input("welcome to our shop, what do you want (c,r,u,d) ? ").lower()
+
+items = ["Tshirt","sweater"]
+
+if action == "r":
+    print("our items : ")
+    print(*items,sep = ",")
+
+elif action == "c":
+    newitems = input("Enter new items :")
+    items.append(newitems)
+    print("our items : ")
+    print(*items, sep = ",")
+
+elif action == "u":
+    pos = int(input("Enter new position :"))
+    newitems = input("Enter new items :")
+    items.insert(newitems)
+
+elif action == "d":
+    deletepos = int(input("Delete position :"))
+    items.pop(deletepos-1)
+    print("our items : ")
+    print(*items, sep=",")
